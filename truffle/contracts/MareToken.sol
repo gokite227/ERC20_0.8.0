af73639281194452c2c379b13215aa2da00994d7
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-12-06 03:30:27 +0900

    Creat/Deploy MareTokenSale

--- truffle/contracts/MareToken.sol
+++ truffle/contracts/MareToken.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MareToken is ERC20 {
+
+    event MarePurchased(address indexed receiver, address indexed buyer);
     constructor(uint256 initialSupply) ERC20("BlueMare", "Mare") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function buyOneMare() public {
+        _burn(_msgSender(), 1);
+        emit MarePurchased(_msgSender(), _msgSender());
+
+    }
+
+    function buyOneMareFrom(address account) public {
+        _spendAllowance(account, _msgSender(), 1);
+        _burn(account, 1);
+        emit MarePurchased(_msgSender(), account);
     }
 }
--- truffle/migrations/2.deploy_MareToken.js
+++ truffle/migrations/2.deploy_MareToken.js
@@ -1,5 +1,9 @@
 const MareToken = artifacts.require("MareToken");
+const MareTokenSale = artifacts.require("MareTokenSale")
 
-module.exports = function (deployer) {
-    deployer.deploy(MareToken, 1000000000);
+module.exports = async function (deployer) {
+    await deployer.deploy(MareToken, 1000000000);
+    await deployer.deploy(MareTokenSale, MareToken.address);
+    const tokenInstance = await MareToken.deployed();
+    await tokenInstance.transfer(MareTokenSale.address, 1000000000);
   };
